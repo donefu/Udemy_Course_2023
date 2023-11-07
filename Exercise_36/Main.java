7ce765d4bd0745dad38fd52329600dcdd9f86b79
Dung Phu <dungphu2k1@gmail.com>
2023-11-07 02:14:26 -0500

    Nov 7

--- Exercise_36/Main.java
+++ Exercise_36/Main.java
@@ -2,7 +2,7 @@ package Exercise_36;
 
 public class Main {
     public static void main(String[] args) {
-        Circle circle = new Circle(3.75);
+        Circle circle = new Circle(4.25);
         System.out.println("circle.radius= " + circle.getRadius());
         System.out.println("circle.area= " + circle.getArea());
         Cylinder cylinder = new Cylinder(5.55, 7.25);
